6f3ad63a824dd31484aa0a4a8cfdfeca4da21630
“Avishek <avishekroy87@gmail.com>
2025-05-18 22:05:32 +0530

    first commitver2

---
+++ helloWorld.py
@@ -0,0 +1,20 @@
+import time
+
+start_time = time.time();
+print("Hello World!");
+
+def add_num(a, b):
+    return a + b
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+result = add_num(num1, num2);
+
+print(result);
+
+for num in range(2, 20000000 , 2):
+    print(num);
+
+
+print("--- %s seconds ---" % (time.time() - start_time));
